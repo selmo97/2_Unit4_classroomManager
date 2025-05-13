af6160ebb1699a6c13d308068d05722b200d30f9
selmo97 <selena.moss2@gmail.com>
2025-05-13 14:58:12 -0700

    seed.js refactored to work w/ Prisma Migrate

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,56 +1,46 @@
 // Clear and repopulate the database.
 
-const db = require("../db");
 const { faker } = require("@faker-js/faker");
+const prisma = require("./index");
 
 async function seed() {
-  console.log("Seeding the database.");
+  console.log("🌱Seeding the database with Prisma");
   try {
     // Clear the database.
-    await db.query("DROP TABLE IF EXISTS student, instructor;");
-
-    // Recreate the tables
-    await db.query(`
-      CREATE TABLE instructor (
-        id SERIAL PRIMARY KEY,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
-      );
-      CREATE TABLE student (
-        id SERIAL PRIMARY KEY,
-        name TEXT NOT NULL,
-        cohort TEXT NOT NULL,
-        instructorId INTEGER NOT NULL REFERENCES instructor(id) ON DELETE CASCADE
-      );
-    `);
+    await prisma.student.deleteMany();
+    await prisma.instructor.deleteMany();
 
     // Add 5 instructors.
-    await Promise.all(
+    const instructors = await Promise.all(
       [...Array(5)].map(() =>
-        db.query(
-          `INSERT INTO instructor (username, password) VALUES ($1, $2);`,
-          [faker.internet.userName(), faker.internet.password()]
-        )
+        prisma.instructor.create({
+          data: {
+            username: faker.internet.userName(),
+            password: faker.internet.password(),
+          },
+        })
       )
     );
 
     // Add 4 students for each instructor.
     await Promise.all(
-      [...Array(20)].map((_, i) =>
-        db.query(
-          `INSERT INTO student (name, cohort, instructorId) VALUES ($1, $2, $3);`,
-          [
-            faker.person.fullName(),
-            faker.number.int({ min: 2000, max: 3000 }),
-            (i % 5) + 1,
-          ]
-        )
+      instructors.flatmap((instructor) =>
+      [...Array(4)].map(() => 
+      prisma.student.create({
+        data: {
+          name: faker.person.fullName(),
+          cohort: faker.number.int({ min: 2000, max: 3000}).toString(),
+          instructorId: instructor.id,
+        },
+      })
       )
-    );
+        )
+      );
+    
 
-    console.log("Database is seeded.");
+    console.log("🌱Database is seeded.");
   } catch (err) {
-    console.error(err);
+    console.error("❌ Seeding error:", err);
   }
 }
 
